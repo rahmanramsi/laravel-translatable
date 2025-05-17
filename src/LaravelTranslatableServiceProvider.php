5a82deec623299555b0d8c0f812a02b726a84f4c
rahmanramsi <48276043+rahmanramsi@users.noreply.github.com>
2025-05-17 01:12:09 +0000

    Fix styling

--- src/LaravelTranslatableServiceProvider.php
+++ src/LaravelTranslatableServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace RahmanRamsi\LaravelTranslatable;
 
+use RahmanRamsi\LaravelTranslatable\Commands\LaravelTranslatableCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use RahmanRamsi\LaravelTranslatable\Commands\LaravelTranslatableCommand;
 
 class LaravelTranslatableServiceProvider extends PackageServiceProvider
 {
